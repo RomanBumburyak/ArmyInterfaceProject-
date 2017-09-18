e306549e9ce696fdcf672b3a075281d21d49785e
RomanBumburyak <romanbumburyak@gmail.com>
2017-09-18 16:15:29 -0400

    Initial commit, all classes made, working on my methods

---
+++ src/Soldier.java
@@ -0,0 +1,52 @@
+public abstract class Soldier {
+    public static void main(String[] args) {
+
+
+        String name;
+        String rank;
+        String serialNumber;
+
+    }
+//////Methods
+
+    public String sleep() {
+        System.out.println(" Sleep / Schlaffen ");
+    }
+
+    public String eat() {
+        System.out.println(" Eat / Essen ");
+    }
+
+    public String walk() {
+        System.out.println(" Walk / Gehen ");
+    }
+
+    /////////Speak Method
+
+    public abstract String speak();
+
+
+
+
+
+
+
+
+
+//    public interface name{
+//
+//
+//    }
+//
+//    public interface rank  {
+//
+//
+//    }
+//
+//    public interface serialNumber{
+//
+//
+//    }
+
+
+}
